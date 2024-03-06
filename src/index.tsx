a588c60ffb4e247235e8780b5b03658fa1f653a8
yuderibolot <yuderibolot@gmail.com>
2024-03-06 19:39:05 +0300

    создание приложения

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+let domNode = document.getElementById('root');
+
+if (!domNode) {
+    domNode = document.createElement('div');
+    domNode.id = 'root';
+    document.body.appendChild(domNode);
+}
+
+const root = createRoot(domNode);
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+    const mode = isProduction ? 'production' : 'development';
+
+    return {
+        mode,
+        entry: {
+            index: path.join(__dirname, 'src', 'index.tsx'),
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        devtool: isProduction ? 'nosources-source-map' : 'source-map',
+        devServer: {
+            static: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                            }
+                        },
+                    ],
+                    exclude: /node-modules/,
+                },
+                {
+                    test: /\.less$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                            }
+                        },
+                        {
+                            loader: 'less-loader',
+                            options: {
+                                lessOptions: {
+                                    globalVars: {
+                                    }
+                                }
+                            }
+                        }
+                    ],
+                    exclude: /node-modules/,
+                },
+                {
+                    test: /\.js$|jsx$|ts$|tsx$/,
+                    use: ['babel-loader'],
+                    exclude: /node-modules/,
+                },
+            ]
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src/')
+            },
+            extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+            symlinks: false,
+            cacheWithContext: false,
+        },
+        optimization: {
+            moduleIds: 'deterministic',
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'initial',
+                    },
+                },
+            },
+        },
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'src', 'index.html')
+            }),
+        ],
+    };
+};
